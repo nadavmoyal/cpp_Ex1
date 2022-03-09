7fe075b3ffbb53bc1d5965c3dbf8e6f2a428ae7e
Nadav Moyal <93326335+nadavmoyal@users.noreply.github.com>
2022-03-09 17:30:49 +0200

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -47,6 +47,7 @@ TEST_CASE("Good input") {
     CHECK(nospaces(mat(1, 1, '$', '0')) == nospaces ("$\n"));
 }
 
+
 TEST_CASE("zero or negative input to 'col' or 'row':") {
     int i= 0;
     while(i>-1000){
@@ -61,33 +62,34 @@ TEST_CASE("zero or negative input to 'col' or 'row':") {
 
 TEST_CASE("even input to 'col' or 'row':") {
     int i=2;
-    while(i<1000){
+    while(i<20){
     CHECK_THROWS(mat(i, 5, '$', '%'));
     CHECK_THROWS(mat(3, i, '!', '$'));
     CHECK_THROWS(mat(i, 9, '*', '@'));
-    CHECK_THROWS(mat(i+1000, 0, '*', 't'));
-    CHECK_THROWS(mat(0, i+1000, '*', 't'));
+    CHECK_THROWS(mat(i+20, 0, '*', 't'));
+    CHECK_THROWS(mat(0, i+20, '*', 't'));
     i=i+2;
     }
     }
-
-TEST_CASE("even input to 'col' or 'row':") {
-    int i=2;
-    while(i<1000){
-    CHECK_THROWS(mat(i, 5, '$', '%'));
-    CHECK_THROWS(mat(3, i, '!', '$'));
-    CHECK_THROWS(mat(i, 9, '*', '@'));
-    CHECK_THROWS(mat(i+1000, 0, '*', 't'));
-    CHECK_THROWS(mat(0, i+1000, '*', 't'));
-    i=i+2;
-    }
-    }
-
-									
+TEST_CASE("Unsupported char - not in the ascii table") {
+    int a = 3; //legal value of col / row
+    int b = 5; //legal value of col / row
+    char c = 126;// this is the last char in ascii 
+    char d = 32; // this is the first char in ascii 
+    c=c+1;   // Unsupported char - not in the ascii table
+    d=d+1;   // Unsupported char - not in the ascii table
+    while(b<20){
+        CHECK_THROWS(mat(a, b, c,d));  
+        a=a+2;
+        b=b+2;
+        c=c+1;
+        d=d-1;
+    }   
+ }								
 TEST_CASE("Bad input") {
     CHECK_THROWS(mat(10, 5, '$', '%'));
-    /* Add more test here */
+    CHECK_THROWS(mat(5, 8, '(', '@'));
+    CHECK_THROWS(mat(10, 4, '$', '!'));
+
 }
 
-
-/* Add more test cases here */
