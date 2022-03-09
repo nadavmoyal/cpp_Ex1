db7cdf651c90149f50d22c730fafd33b5846b53d
Nadav Moyal <93326335+nadavmoyal@users.noreply.github.com>
2022-03-09 17:40:37 +0200

    Final

--- Test.cpp
+++ Test.cpp
@@ -42,16 +42,14 @@ TEST_CASE("Good input") {
 													 "xxxxxxx\n"));
     CHECK(nospaces(mat(1, 1, '$', '0')) == nospaces ("$\n"));
 }
-
-
 TEST_CASE("zero or negative input to 'col' or 'row':") {
     int i= 0;
-    while(i>-1000){
+    while(i>-10){
     CHECK_THROWS(mat(i, 5, '$', '%'));
     CHECK_THROWS(mat(3, i, '!', '$'));
     CHECK_THROWS(mat(i, 9, '*', '@'));
-    CHECK_THROWS(mat(i+1000, 0, '*', 't'));
-    CHECK_THROWS(mat(0, i+1000, '*', 't'));
+    CHECK_THROWS(mat(i+10, 0, '*', 't'));
+    CHECK_THROWS(mat(0, i+10, '*', 't'));
     i=i-1;
     }
     }
@@ -62,11 +60,17 @@ TEST_CASE("even input to 'col' or 'row':") {
     CHECK_THROWS(mat(i, 5, '$', '%'));
     CHECK_THROWS(mat(3, i, '!', '$'));
     CHECK_THROWS(mat(i, 9, '*', '@'));
-    CHECK_THROWS(mat(i+20, 0, '*', 't'));
+    CHECK_THROWS(mat(i+10, 0, '*', 't'));
     CHECK_THROWS(mat(0, i+20, '*', 't'));
     i=i+2;
     }
-    }
+    }	
+TEST_CASE("Bad input") {
+    CHECK_THROWS(mat(10, 5, '$', '%'));
+    CHECK_THROWS(mat(5, 8, '(', '@'));
+    CHECK_THROWS(mat(10, 4, '$', '!'));
+
+}
 TEST_CASE("Unsupported char - not in the ascii table") {
     int a = 3; //legal value of col / row
     int b = 5; //legal value of col / row
@@ -76,16 +80,9 @@ TEST_CASE("Unsupported char - not in the ascii table") {
     d=d+1;   // Unsupported char - not in the ascii table
     while(b<20){
         CHECK_THROWS(mat(a, b, c,d));  
-        a=a+2;
-        b=b+2;
+        a=a+4;
+        b=b+4;
         c=c+1;
         d=d-1;
     }   
- }								
-TEST_CASE("Bad input") {
-    CHECK_THROWS(mat(10, 5, '$', '%'));
-    CHECK_THROWS(mat(5, 8, '(', '@'));
-    CHECK_THROWS(mat(10, 4, '$', '!'));
-
-}
-
+ }						
